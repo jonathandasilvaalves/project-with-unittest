e1fbe180028eb9b93ae5b715b7948818e13f98c1
Jonathan da Silva Alves <jonathandasilvaalves@Jonathans-MacBook-Air.local>
2023-11-23 07:25:11 -0300

    Configure settings for the database and user model

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV == 'test' ? '.env.test': '.env'
+});
+
 module.exports = {
-  host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
-  database: 'nodeauth',
-  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || 'postgres',
+  storage: './__tests__/this.database.sqlite',
   operatorsAliases: false,
   logging: false,
   define: {
@@ -11,4 +17,4 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
   }
-}
+};
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
 const routes = require("express").Router();
 
+const authMiddleware = require("./app/middleware/auth")
+
+const SessionController = require("./app/controllers/SessionController");
+
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
 module.exports = routes;
